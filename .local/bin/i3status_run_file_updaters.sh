09236bba0697e2e0d008777f597a5a241738bfbb
Witalis Domitrz <witalis@x1>
2024-05-20 00:19:38 +0100

    some changes

--- .local/bin/i3status_run_file_updaters.sh
+++ .local/bin/i3status_run_file_updaters.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
-file_updaters=()
 
 function main() {
-    for what in "${file_updaters[@]}"; do
+    set -euo pipefail
+    file_updaters_dir="${HOME}"/.local/bin/i3status_file_updaters/
+    check_integrity_of_tracked_dir "${file_updaters_dir}" || exit 1
+
+    readarray -t file_updaters < <(
+        find "${file_updaters_dir}" -maxdepth 1 -not -name '.*' -type f -follow
+    )
+
+    for script_path in "${file_updaters[@]}"; do
+        what="$(basename --suffix=".sh" "${script_path}")"
         while :; do
-            i3status_file_updaters_"${what}".sh | sponge "${HOME}"/.cache/i3status_files-"${what}".txt
+            "${script_path}" | sponge "${HOME}"/.cache/i3status_files-"${what}".txt
             sleep 10
         done &
     done

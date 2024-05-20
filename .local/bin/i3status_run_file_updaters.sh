b8600430047f64ed0bbcef054025a102b28fe7ce
Witalis Domitrz <witalis@x1>
2024-05-20 23:01:39 +0100

    some changes

--- .local/bin/i3status_run_file_updaters.sh
+++ .local/bin/i3status_run_file_updaters.sh
@@ -1,23 +1,17 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
-function main() {
-    set -euo pipefail
-    file_updaters_dir="${HOME}"/.local/bin/i3status_file_updaters/
-    check_integrity_of_tracked_dir "${file_updaters_dir}" || exit 1
+file_updaters_dir="${HOME}"/.local/bin/i3status_file_updaters/
+check_integrity_of_tracked_dir "${file_updaters_dir}" || exit 1
 
-    readarray -t file_updaters < <(
-        find "${file_updaters_dir}" -maxdepth 1 -not -name '.*' -type f -follow
-    )
+readarray -t file_updaters < <(
+    find "${file_updaters_dir}" -maxdepth 1 -not -name '.*' -type f -follow
+)
 
-    for script_path in "${file_updaters[@]}"; do
-        what="$(basename --suffix=".sh" "${script_path}")"
-        while :; do
-            "${script_path}" | sponge "${HOME}"/.cache/i3status_files-"${what}".txt
-            sleep 10
-        done &
-    done
-}
-
-if [[ "$#" -ne 1 ]] || [[ "${1}" != "--source-only" ]]; then
-    main "${@}"
-fi
+for script_path in "${file_updaters[@]}"; do
+    what="$(basename --suffix=".sh" "${script_path}")"
+    while :; do
+        "${script_path}" | sponge "${HOME}"/.cache/i3status_files-"${what}".txt
+        sleep 10
+    done &
+done

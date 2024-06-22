f278d0b25035901663fb3a7c9cfae31b1c7021b8
Witalis Domitrz <witekdomitrz@gmail.com>
2024-06-22 13:35:00 +0100

    _

--- .local/bin/i3status_run_file_updaters.sh
+++ .local/bin/i3status_run_file_updaters.sh
@@ -2,6 +2,8 @@
 set +e
 set -uo pipefail
 
+kill_others_with_given_name "$(basename "$0")"
+
 file_updaters_dir="${HOME}"/.local/bin/i3status_file_updaters/
 check_integrity_of_tracked_dir "${file_updaters_dir}" || exit 1
 

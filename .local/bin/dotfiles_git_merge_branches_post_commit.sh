6cec4a8440cf1651c71a7eb0e0a56c93b2d20f88
Witalis Domitrz <witekdomitrz@gmail.com>
2024-10-10 20:48:47 +0100

    some changes

--- .local/bin/dotfiles_git_merge_branches_post_commit.sh
+++ .local/bin/dotfiles_git_merge_branches_post_commit.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ue
 
-MAIN_BRANCH="$(cat "${HOME}"/.config/git/dotfiles/main_branch.sorted.txt)"
+readonly MAIN_BRANCH="$(cat "${HOME}"/.config/git/dotfiles/main_branch.sorted.txt)"
 readarray -t ALL_BRANCHES < "${HOME}"/.config/git/dotfiles/all_branches.sorted.txt
 
 function merge_with_main_branch {
@@ -13,7 +13,7 @@ function merge_with_main_branch {
 function go_back_push_and_cleanup {
     git checkout "${MAIN_BRANCH}"
     git push --all --force-with-lease
-    git branch -d "${MAIN_BRANCH}" "${ALL_BRANCHES[@]}" || echo "Branches removed"
+    git branch --delete "${MAIN_BRANCH}" "${ALL_BRANCHES[@]}" || echo "Branches removed"
 }
 
 if [[ "${MAIN_BRANCH}" != "$(git rev-parse --abbrev-ref HEAD)" ]]; then

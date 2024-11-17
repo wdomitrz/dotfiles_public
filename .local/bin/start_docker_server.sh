e08ae61d2d7fd8a91cb78d9faf0196bbec7cddfc
Witalis Domitrz <witekdomitrz@gmail.com>
2024-11-17 13:16:07 +0000

    some changes

--- .local/bin/start_docker_server.sh
+++ .local/bin/start_docker_server.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
 function start_docker_server() {
-    docker build --tag my - < "${HOME}"/.config/docker/my.dockerfile &&
+    suffix="$1"
+    if [[ ! -z "${suffix}" ]]; then
+        suffix=_"${suffix}"
+    fi
+    docker_file="${HOME}"/.config/docker/my"${suffix}".dockerfile
+
+    [[ -f "${docker_file}" ]] || (echo "No dockerfile '${docker_file}'file found" && exit 1)
+
+    docker build --tag my"${suffix}":local - < "${docker_file}" &&
         echo "Starting ssh server. Press Ctrl-C to stop it" &&
-        docker run --publish 2222:22 --tty=true --interactive=true my
+        docker run --publish 2222:22 --tty=true --interactive=true my"${suffix}":local
 }
 export -f start_docker_server
 
-tmux new -d "start_docker_server"
+if [[ "$#" -ne 1 ]] || [[ "${1}" != "--source-only" ]]; then
+    tmux new -d "start_docker_server" "$@"
+fi

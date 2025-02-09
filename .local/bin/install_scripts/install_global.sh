d4de8aaf2e3c7e9d11bcbaee754fca1a8cf40530
Witalis Domitrz <witekdomitrz@gmail.com>
2025-02-09 01:30:08 +0000

    some changes

--- .local/bin/install_scripts/install_global.sh
+++ .local/bin/install_scripts/install_global.sh
@@ -4,21 +4,61 @@ function install_deb_from_url() {
     link="$1"
     package_file="$(mktemp /tmp/XXXXXX.deb)"
     wget_with_defaults.sh --max-redirect=1 "${link}" > "${package_file}"
-    sudo apt-get install --yes "${package_file}"
+    sudo apt-get install --yes --no-install-recommends "${package_file}"
     rm "${package_file}"
 }
 
-function enable_sources_file() {
+function enable_source_and_list_packages() {
     set -euo pipefail
     sources_file=/etc/apt/sources.list.d/"$1"
 
-    key_source=$(grep 'Key-Source: ' "${sources_file}" | sed 's/Key-Source: //g')
-    key_file=$(grep 'Signed-By: ' "${sources_file}" | sed 's/Signed-By: //g')
+    if grep --quiet '^Enabled: yes$' "${sources_file}"; then
+        echo 1>&2 "${sources_file} already enabled"
+        return 0
+    fi
+    if grep --quiet '^Signed-By:$' "${sources_file}"; then
+        echo 1>&2 "${sources_file} already has Signed-By section. Invalid format."
+        return 1
+    fi
 
-    sudo mkdir --parents "$(dirname "${key_file}")"
-    wget_with_defaults.sh "${key_source}" | sudo sponge "${key_file}"
+    key_source=$(grep 'key_source: ' "${sources_file}" | sed 's/key_source: //g')
+    (
+        echo "Signed-By:"
+        wget_with_defaults.sh "${key_source}" | sponge |
+            sed --expression='s|^$|.|g' --expression='s|^| |g'
+    ) | sudo sponge -a "${sources_file}"
 
     sudo sed --in-place 's/^Enabled: no$/Enabled: yes/g' "${sources_file}"
+
+    (grep '^packages_to_install: ' "${sources_file}" || true) |
+        sed 's/packages_to_install: //g'
+}
+
+function enable_sources_and_install_packages() {
+    packages_to_install=""
+    while [[ $# -gt 0 ]]; do
+        packages_from_this_source="$(enable_source_and_list_packages "$1")"
+        packages_to_install="${packages_to_install} ${packages_from_this_source}"
+        shift 1
+    done
+    read -r -a packages_to_install <<< "${packages_to_install}"
+
+    sudo apt-get update --yes &&
+        sudo apt-get install --yes "${packages_to_install[@]}"
+}
+
+function install_packages_external() {
+    enable_sources_and_install_packages \
+        google_chrome.sources \
+        nordvpn.sources \
+        signal.sources \
+        tailscale.sources \
+        vscode.sources
+}
+
+function install_chrome_remote_desktop() {
+    enable_sources_and_install_packages chrome_remote_desktop.sources
+    echo "Configure Chrome Remote Desktop at" "https://remotedesktop.google.com/headless"
 }
 
 function not_sudo() {
@@ -49,29 +89,10 @@ function install_dust() {
     install_deb_from_url "https://github.com/bootandy/dust/releases/download/v1.1.1/du-dust_1.1.1-1_amd64.deb"
 }
 
-function install_chrome_remote_desktop() {
-    enable_sources_file chrome_remote_desktop.sources
-    sudo apt-get update --yes &&
-        sudo apt-get install --yes chrome-remote-desktop
-    # Configure Chrome Remote Desktop
-    echo "Configure Chrome Remote Desktop at" "https://remotedesktop.google.com/headless"
-}
-
 function install_usb_c_display_driver() {
     install_deb_from_url https://www.synaptics.com/sites/default/files/Ubuntu/pool/stable/main/all/synaptics-repository-keyring.deb
     sudo apt-get update --yes &&
         sudo apt-get install --yes displaylink-driver evdi-dkms
-}
-
-function install_packages_external() {
-    enable_sources_file google_chrome.sources
-    enable_sources_file signal.sources
-    enable_sources_file tailscale.sources
-    enable_sources_file vscode.sources
-
-    source "${HOME}"/.local/bin/install_scripts/install_packages.sh --source-only &&
-        sudo apt-get update --yes &&
-        install_packages_from "${HOME}"/.config/packages/packages_external.sorted.txt
 }
 
 function install_global_main() {

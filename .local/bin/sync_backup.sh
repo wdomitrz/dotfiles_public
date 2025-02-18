ad1ed1b08e3db42f4c952efc245d921f7717fd0e
Witalis Domitrz <witekdomitrz@gmail.com>
2025-02-18 22:39:07 +0000

    some changes

--- .local/bin/sync_backup.sh
+++ .local/bin/sync_backup.sh
@@ -1,6 +1,11 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+set -euo pipefail
+
+from_location="$1/data/"
+to_location="$2/data/"
+
 rsync \
     --archive --verbose \
     --partial --progress \
     --exclude Oxford \
-    "$@"
+    "${from_location}" "${to_location}"

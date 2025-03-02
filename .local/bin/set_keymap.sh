b2ab008b18263697ad04403ab714bf430cf48756
Witalis Domitrz <witekdomitrz@gmail.com>
2025-03-02 01:16:52 +0000

    some changes

--- .local/bin/set_keymap.sh
+++ .local/bin/set_keymap.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env sh
 
+# Clear all options
 pkill -9 --full xcape || true
+xmodmap -e "clear lock"
 
-# caps to ctrl
-setxkbmap -option
+# caps -> esc ctrl
 setxkbmap -option ctrl:nocaps
-
-# ctrl as esc when tapped
 xcape -e 'Control_L=Escape'
 
-# space as win
-xmodmap -e "clear lock"
+# space win
 xmodmap -e "keycode 65 = Super_L"
+xmodmap -e "keycode any = space" && xcape -e 'Super_L=space'
 
-# win as space
-xmodmap -e "keycode any = space"
-xcape -e 'Super_L=space'
+# tab alt
+xmodmap -e "keycode 23 = Alt_L"
+xmodmap -e "keycode any = Tab" && xcape -e 'Alt_L=Tab'

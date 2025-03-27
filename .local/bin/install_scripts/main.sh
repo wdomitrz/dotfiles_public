4356b5dd31e6d8eab904ce71c11e3768947afac6
Witalis Domitrz <witekdomitrz@gmail.com>
2025-03-27 21:54:24 +0000

    some changes

--- .local/bin/install_scripts/main.sh
+++ .local/bin/install_scripts/main.sh
@@ -2,7 +2,6 @@
 set -euo pipefail
 set -x
 
-source "${HOME}"/.local/bin/install_scripts/clear_packages.sh --source-only
 source "${HOME}"/.local/bin/install_scripts/config_global.sh --source-only
 source "${HOME}"/.local/bin/install_scripts/config_user.sh --source-only
 source "${HOME}"/.local/bin/install_scripts/install_global.sh --source-only
@@ -12,7 +11,6 @@ source "${HOME}"/.local/bin/install_scripts/install_user.sh --source-only
     source "${HOME}"/.local/bin/install_scripts/machine_specific.sh --source-only
 
 install_packages_main
-clear_packages_main
 install_global_main
 # part of config_global_main which is not used in install_packages_main
 config_global_rest

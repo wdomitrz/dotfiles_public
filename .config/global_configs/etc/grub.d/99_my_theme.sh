9834af0beea1853cd6ed092a5d10c28ca711f2f3
Witalis Domitrz <witekdomitrz@gmail.com>
2025-04-06 17:48:45 +0100

    _

--- .config/global_configs/etc/grub.d/99_my_theme.sh
+++ .config/global_configs/etc/grub.d/99_my_theme.sh
@@ -1,2 +1,5 @@
+#!/usr/bin/env bash
+echo -n "\
 set menu_color_normal=white/black
 set menu_color_highlight=black/light-gray
+"
--- .local/bin/install_scripts/install_packages.sh
+++ .local/bin/install_scripts/install_packages.sh
@@ -10,12 +10,20 @@ function install_packages_from() {
     xargs sudo DEBIAN_FRONTEND=noninteractive apt-get install --yes --no-install-recommends < "${packages_file}"
 }
 
-function install_packages_base() {
-    install_packages_from "${HOME}"/.config/packages/packages.sorted.txt
+function install_packages_from_main() {
+    install_packages_from "${HOME}"/.config/packages/packages_main.sorted.txt
 }
 
-function install_packages_other() {
+function install_packages_from_other() {
     install_packages_from "${HOME}"/.config/packages/packages_other.sorted.txt
+}
+
+function install_packages_from_nvidia() {
+    if ! (lspci | grep --quiet --ignore-case nvidia); then
+        echo "nvidia driver not needed"
+        return 0
+    fi
+    install_packages_from "${HOME}"/.config/packages/packages_nvidia.sorted.txt
 }
 
 function install_packages_main() {
@@ -23,11 +31,13 @@ function install_packages_main() {
     set -x
     source "${HOME}"/.local/bin/install_scripts/config_global.sh --source-only
 
+    copy_global_configs
     update_and_upgrade
-    install_packages_base
+    install_packages_from_main
     config_global_start
     update_and_upgrade
-    install_packages_other
+    install_packages_from_other
+    install_packages_from_nvidia
 }
 
 if [[ $# -ne 1 ]] || [[ ${1} != "--source-only" ]]; then

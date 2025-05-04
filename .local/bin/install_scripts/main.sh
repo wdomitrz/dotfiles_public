52a869b50d681784923c76222fefe0d0ba20c16d
Witalis Domitrz <witekdomitrz@gmail.com>
2025-05-04 03:14:52 +0100

    _

--- .local/bin/install_scripts/main.sh
+++ .local/bin/install_scripts/main.sh
@@ -7,5 +7,6 @@ set -x
 "${HOME}"/.local/bin/install_scripts/config_global.sh
 "${HOME}"/.local/bin/install_scripts/install_user.sh
 "${HOME}"/.local/bin/install_scripts/config_user.sh
-[[ -f "${HOME}"/.local/bin/install_scripts/machine_specific.sh ]] &&
+if [[ -f "${HOME}"/.local/bin/install_scripts/machine_specific.sh ]]; then
     "${HOME}"/.local/bin/install_scripts/machine_specific.sh
+fi

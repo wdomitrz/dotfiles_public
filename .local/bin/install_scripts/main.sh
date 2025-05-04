0f5371aaa4efd2d9659d35751dd6a5ee1cf84a6d
Witalis Domitrz <witekdomitrz@gmail.com>
2025-05-04 02:50:51 +0100

    _

--- .local/bin/install_scripts/main.sh
+++ .local/bin/install_scripts/main.sh
@@ -2,19 +2,10 @@
 set -euo pipefail
 set -x
 
-source "${HOME}"/.local/bin/install_scripts/config_global.sh --source-only
-source "${HOME}"/.local/bin/install_scripts/config_user.sh --source-only
-source "${HOME}"/.local/bin/install_scripts/install_global.sh --source-only
-source "${HOME}"/.local/bin/install_scripts/install_packages.sh --source-only
-source "${HOME}"/.local/bin/install_scripts/install_user.sh --source-only
+"${HOME}"/.local/bin/install_scripts/install_packages.sh
+"${HOME}"/.local/bin/install_scripts/install_global.sh
+"${HOME}"/.local/bin/install_scripts/config_global.sh
+"${HOME}"/.local/bin/install_scripts/install_user.sh
+"${HOME}"/.local/bin/install_scripts/config_user.sh
 [[ -f "${HOME}"/.local/bin/install_scripts/machine_specific.sh ]] &&
-    source "${HOME}"/.local/bin/install_scripts/machine_specific.sh --source-only
-
-install_packages_main
-install_global_main
-# part of config_global_main which is not used in install_packages_main
-config_global_rest
-install_user_main
-config_user_main
-[[ -f "${HOME}"/.local/bin/install_scripts/machine_specific.sh ]] &&
-    machine_specific_main
+    "${HOME}"/.local/bin/install_scripts/machine_specific.sh

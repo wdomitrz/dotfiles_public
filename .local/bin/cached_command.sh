c383ecc5815d3f6f7524d6555c6f07d59d0651c5
Witalis Domitrz <witekdomitrz@gmail.com>
2025-06-01 21:34:01 +0100

    some changes

--- .local/bin/cached_command.sh
+++ .local/bin/cached_command.sh
@@ -2,36 +2,35 @@
 set -euo pipefail
 
 function usage_and_exit() {
-    echo "usage: $(basename "$0") [-h|--help] [--mtime_cache_too_old <mtime_cache_too_old>] <command> [<command_params>]"
+    echo "usage: $(basename "$0") [-h|--help] [--overwrite] <command> [<command_params>]"
     exit "$1"
 }
+
+overwrite=false
+case "$1" in
+-h | --help)
+    usage_and_exit 0
+    ;;
+--overwrite)
+    overwrite=true
+    shift 1
+    ;;
+*) ;;
+esac
 
 if [[ $# -eq 0 ]]; then
     usage_and_exit 1
 fi
 
-mtime_cache_too_old="+1"
-case "$1" in
--h | --help)
-    usage_and_exit 0
-    ;;
---mtime_cache_too_old)
-    if [[ $# -lt 2 ]]; then
-        usage_and_exit 1
-    fi
-    mtime_cache_too_old="$2"
-    shift 2
-    ;;
-*) ;;
-esac
+base_cache_dir="/run/user/$(id -u "${USER}")"
+
+if [[ ! -d ${base_cache_dir} ]]; then exec "$@"; fi
 
 args_hash="$(printf '%s\t\0' "$@" | md5sum | awk '{print $1}')"
-cache_file="${HOME}/.cache/cached_command/cached_command_file_${args_hash}.txt"
+cache_file="${base_cache_dir}/cached_command/${args_hash}.txt"
 
-if [[ ! -f ${cache_file} ]] ||
-    [[ -n "$(find "${cache_file}" -mtime "${mtime_cache_too_old}")" ]]; then
+if "${overwrite}" || [[ ! -f ${cache_file} ]]; then
     # shellcheck disable=SC2174
-    # --mode=700 applied only to the deepest directory - as intended
     mkdir --parents --mode=700 "$(dirname "${cache_file}")"
 
     touch "${cache_file}"

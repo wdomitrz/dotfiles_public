e1a02ea1b2c1deb205a4a8469d18214a91858b30
Witalis Domitrz <witekdomitrz@gmail.com>
2025-10-28 01:12:59 +0000

    _

--- .local/bin/propagate_config_to.sh
+++ .local/bin/propagate_config_to.sh
@@ -6,6 +6,6 @@ destination="$1"
 ssh-copy-id "${destination}"
 scp "${HOME}"/.ssh/authorized_keys "${destination}":~/.ssh/authorized_keys
 scp \
-  "${HOME}"/.bashrc "${HOME}"/.bash_aliases "${HOME}"/.profile \
+  "${HOME}"/.bashrc "${HOME}"/.profile \
   "${HOME}"/.local/bin/.config_sshd.sh \
   "${destination}":
